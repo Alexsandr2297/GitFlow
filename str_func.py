1704975f145c5b6e4a0303fc736069a45e3638ab
Alexsandr2297 <162041605+Alexsandr2297@users.noreply.github.com>
2024-03-16 16:20:51 +0300

    str_func.py

--- str_func.py
+++ str_func.py
@@ -7,16 +7,3 @@ def string():
 string()
 
 
-def word_str(string):
-    """ функция которая делает заглавные первые буквы каждого слова в строке """
-    words = string.split(" ")
-    result = ""
-    for word in words:
-        word = word.capitalize()
-        result += " " + word
-
-    return result
-
-
-string = "hello world"
-print(word_str(string))
