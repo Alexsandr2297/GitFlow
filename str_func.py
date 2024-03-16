5ff18e7c4e985f95b1786f2f8bd142ff932c48f7
Alexsandr Varenyshev <warrn11@mail.ru>
2024-03-16 16:42:38 +0300

    conflict str_func.py

--- str_func.py
+++ str_func.py
@@ -1,6 +1,21 @@
 def string():
+    """Фунция переписывает в заглавные букв"""
     text = input().upper()
     print(text)
 
 
 string()
+
+
+def capitalize_words(string):
+    """функция которая делает заглавные первые буквы каждого слова в строке"""
+    words = string.split(" ")
+    result = ""
+    for word in words:
+        word = word.capitalize()
+        result += " " + word
+    return result
+
+
+string = "hello world"
+print(capitalize_words(string))
